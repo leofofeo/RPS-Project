1c9f7669959024057c07d823d15261ce43ac6a8b
Daniel Nolan <DanNolan99@gmail.com>
2020-05-31 22:40:45 -0400

    adds client/server

--- client/index.js
+++ client/index.js
@@ -1,1 +1,26 @@
-console.log('hello world!')
+import "./index.scss";
+
+let ws;
+function start(){
+    ws = new WebSocket('ws://localhost:8080');
+    ws.onmessage = function({ data }) {
+        document.getElementById("game").innerHTML = data;
+    };
+    ws.onclose = function(){
+        // attempt reconnection on close
+        setTimeout(start, 1000);
+    };
+}
+start();
+
+console.log(document.getElementById("player-one-send"));
+
+document.getElementById("player-one-send").addEventListener('click', () => {
+    const {value} = document.getElementById("player-one-select");
+    ws.send(JSON.stringify({ player: 0, move: value }));
+});
+
+document.getElementById("player-two-send").addEventListener('click', () => {
+    const {value} = document.getElementById("player-two-select");
+    ws.send(JSON.stringify({ player: 1, move: value }));
+});
---
+++ server/index.js
@@ -0,0 +1,48 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+const game = {
+  player1Move: null,
+  player2Move: null,
+  result: null,
+}
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    const { player, move } = JSON.parse(data);
+    if(player === 0) {
+        game.player1Move = move;
+    }
+    else if(player === 1) {
+        game.player2Move = move;
+    }
+    if(game.player1Move && game.player2Move) {
+        calculateWinner();
+    }
+    ws.send(JSON.stringify(game));
+  });
+
+  ws.send(JSON.stringify(game));
+});
+
+const wins = [
+  ['rock', 'scissors'],
+  ['scissors', 'paper'],
+  ['paper', 'rock'],
+];
+
+function calculateWinner() {
+    const {player1Move, player2Move} = game;
+    if(player1Move === player2Move) {
+        game.result = "tie!";
+        return;
+    }
+    const p1Win = wins.find(([p1, p2]) => player1Move === p1 && player2Move === p2);
+    if(p1Win) {
+        game.result = "Player One Wins!";
+    }
+    else {
+        game.result = "Player Two Wins!";
+    }
+}
